c2278ea4afb9d6910a02eb60900a8c56f897aae5
Jongbin Jung <jongbin@stanford.edu>
2016-12-07 12:22:04 -0800

    Init rmarkdown blogging w/ custom build file

---
+++ build.R
@@ -0,0 +1,28 @@
+#!/usr/bin/env Rscript
+
+library(tools)
+library(knitr)
+opts_knit$set(
+  unnamed.chunk.label='plots',
+  base.dir='static/img/posts/R',
+  base.url='/img/posts/R/'
+  )
+source <- './rsrc'
+target <- './content/post'
+r <- sapply(list.files(source), function (rmd) {
+  input_file <- file.path(source, rmd)
+  output_file <- file.path(target, paste0(file_path_sans_ext(rmd), '.md'))
+  if (as.POSIXlt(file.mtime(input_file)) > as.POSIXlt(file.mtime(output_file))) {
+    cat(sprintf('Rebuild %s -> %s\n', input_file, output_file))
+    knitr::knit(
+      input=input_file,
+      output=output_file,
+      quiet=TRUE,
+      encoding='utf-8'
+    )
+    return(TRUE)
+  }
+  return(FALSE)
+})
+
+cat(sprintf('Updated total %d pages\n', sum(r)))
